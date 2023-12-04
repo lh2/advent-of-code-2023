e13fadabbc3961d0288b7b894ed49eabe8c7c194
Lukas Henkel <lh@entf.net>
2023-12-04 06:40:04 +0100

    Day 4 task 2

--- src/day-4.lisp
+++ src/day-4.lisp
@@ -34,9 +34,36 @@
                              (* score 2)))
         finally (return score)))
 
+(declaim (ftype (function (list list) fixnum) card-matching-numbers))
+(defun card-matching-numbers (winning-numbers my-numbers)
+  (loop for number fixnum in my-numbers
+        when (member number winning-numbers)
+          sum 1 fixnum))
+
+(declaim (ftype (function (list) (values fixnum list)) process-copies))
+(defun process-copies (copies)
+  (loop with total-copies fixnum = 0
+        with new-copies = nil
+        for copy fixnum in copies
+        do (decf copy)
+        when (>= copy 0)
+          do (incf total-copies)
+        when (> copy 0)
+          do (push copy new-copies)
+        finally (return (values total-copies new-copies))))
+
 (defun day-4 (input)
-  (loop for line = (read-line input nil)
+  (loop with copies = nil
+        with task-2 fixnum = 0
+        for line = (read-line input nil)
         while line
         for (winning-numbers my-numbers) = (read-card-numbers line)
+        for matching-numbers = (card-matching-numbers winning-numbers my-numbers)
+        do (multiple-value-bind (this-card-copies new-copies)
+               (process-copies copies)
+             (incf task-2 (1+ this-card-copies))
+             (setf copies new-copies)
+             (loop for copy from 0 to this-card-copies
+                   do (push matching-numbers copies)))
         sum (task-1 winning-numbers my-numbers) into task-1 fixnum
-        finally (return (values task-1))))
+        finally (return (values task-1 task-2))))
