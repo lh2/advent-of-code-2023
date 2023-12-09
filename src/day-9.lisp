d23cd36cd3c107662e3ba682530ec7350c7b9a71
Lukas Henkel <lh@entf.net>
2023-12-09 06:15:26 +0100

    Day 9 task 1

---
+++ src/day-9.lisp
@@ -0,0 +1,20 @@
+(defpackage #:aoc/day-9
+  (:use #:cl #:aoc/utils)
+  (:export #:day-9))
+(in-package #:aoc/day-9)
+
+(defun find-next-value (numbers)
+  (let ((new-sequence (loop with last = (first numbers)
+                            for number in (rest numbers)
+                            collect (- number last)
+                            do (setf last number))))
+    (if (every (curry #'= 0) new-sequence)
+        (first numbers)
+        (+ (lastcar numbers)
+           (find-next-value new-sequence)))))
+
+(defun day-9 (input)
+  (loop for line = (read-line input nil)
+        while line
+        for numbers = (read-number-list line)
+        sum (find-next-value numbers) fixnum))
---
+++ t/day-9.lisp
@@ -0,0 +1,11 @@
+(defpackage #:aoc-test/day-9
+  (:use #:cl #:lisp-unit2))
+(in-package #:aoc-test/day-9)
+
+(define-test test-day-9
+    ()
+  (multiple-value-bind (task-1)
+      (aoc:run-day 9 "0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45")
+    (assert= 114 task-1)))
