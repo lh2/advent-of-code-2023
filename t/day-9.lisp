79070780854b628e3aec5095064ead5a73e2971b
Lukas Henkel <lh@entf.net>
2023-12-09 06:32:25 +0100

    Add test for task 2

--- t/day-9.lisp
+++ t/day-9.lisp
@@ -4,8 +4,9 @@
 
 (define-test test-day-9
     ()
-  (multiple-value-bind (task-1)
+  (multiple-value-bind (task-1 task-2)
       (aoc:run-day 9 "0 3 6 9 12 15
 1 3 6 10 15 21
 10 13 16 21 30 45")
-    (assert= 114 task-1)))
+    (assert= 114 task-1)
+    (assert= 2 task-2)))
