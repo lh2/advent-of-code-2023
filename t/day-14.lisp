69e1a93bd58be13057b4f169cac2d3c4735d337a
Lukas Henkel <lh@entf.net>
2023-12-14 20:36:22 +0100

    :use aoc/day-14

--- t/day-14.lisp
+++ t/day-14.lisp
@@ -1,10 +1,10 @@
 (defpackage #:aoc-test/day-14
-  (:use #:cl #:lisp-unit2))
+  (:use #:cl #:lisp-unit2 #:aoc/day-14))
 (in-package #:aoc-test/day-14)
 
 (define-test test-day-14
     ()
-  (let ((aoc/day-14:*minimum-pattern-length* 5))
+  (let ((*minimum-pattern-length* 5))
     (multiple-value-bind (task-1 task-2)
         (aoc:run-day 14 "O....#....
 O.OO#....#
